f86632e65500b22a5e277263d638e04a9543c64d
martezdaniel <155940648+martezdaniel@users.noreply.github.com>
2025-01-11 01:08:15 +0000

    Jan 10, 2025, 8:08 PM

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // reference for navbar javascript: https://www.youtube.com/watch?v=At4B7A4GOPg https://www.w3schools.com/howto/howto_js_topnav.asp
 
-// gets the element with the class name toggle-button (the hamburger icon)
+// gets the element from the class with the toggle button in the nav bar (the hamburger icon)
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 // gets the element from the class with the navigation bar
+const navBarLinks = document.getElementsByClassName('navbar-links')[0];
