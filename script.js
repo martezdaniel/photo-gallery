70398fc4e7090513473f88616238535da16f535c
martezdaniel <155940648+martezdaniel@users.noreply.github.com>
2025-01-13 08:33:46 +0000

    Jan 13, 2025, 3:33 AM

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // reference for navbar javascript: https://www.youtube.com/watch?v=At4B7A4GOPg https://www.w3schools.com/howto/howto_js_topnav.asp
 // reference for lightbox effect: https://www.youtube.com/watch?v=uKVVSwXdLr0
 
-// code for the toggle button
+// code for the hamburger toggle button in the nav bar (for mobile view) //
 
 // gets the element from the class with the toggle button in the nav bar (the hamburger icon)
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
@@ -13,3 +13,9 @@ toggleButton.addEventListener('click', () => {
     // when the icon is clicked, the active class will be toggled on if it isn't already, meaning that nav bar will appear, and vice-versa
 })
 
+// code for the lightbox effect for the gallery images //
+
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
