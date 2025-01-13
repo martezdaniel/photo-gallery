ea507ca8c25fe2a70884b7d7602e5189b57c42d7
martezdaniel <155940648+martezdaniel@users.noreply.github.com>
2025-01-13 17:09:32 +0000

    Jan 13, 2025, 12:09 PM

--- script.js
+++ script.js
@@ -22,3 +22,5 @@ document.body.appendChild(lightbox); // appends the lightbox to the end of the d
 
 // selects all the images from the gallery-grid class so the code only affects the images from the galleries
 const images = document.querySelectorAll('.gallery-grid img');
+
+// finish here with adding the click event listener and the function for the user to toggle the lightbox when they click an image //
